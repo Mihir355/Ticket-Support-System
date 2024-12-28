8d37034d2a7d660d286ac9914e0a783e01d4d871
Mihir Biswas <biswasmihir355@gmail.com>
2024-12-28 16:02:51 +0530

    modified homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,26 +6,33 @@ const Homepage = () => {
   return (
     <div className="main-container">
       <header className="hero-section">
-        <h1 className="hero-title">Welcome to the Product Support Platform</h1>
-        <p className="hero-description">
-          Simplify your support needs with our platform. Whether you're a user
-          or an employee, we provide the tools to help you connect, resolve
-          issues, and get the support you need.
-        </p>
-        <img
-          src="https://res.cloudinary.com/dkpm0glt6/image/upload/v1735365050/Free_Vector___Flat_design_illustration_customer_support_shac43.jpg"
-          alt="Support Illustration"
-          className="hero-image"
-        />
+        <div className="hero-left">
+          <h1 className="hero-title">
+            Welcome to the Product Support Platform
+          </h1>
+          <p className="hero-description">
+            Simplify your support needs with our platform. Whether you're a user
+            or an employee, we provide the tools to help you connect, resolve
+            issues, and get the support you need.
+          </p>
+          <img
+            src="https://res.cloudinary.com/dkpm0glt6/image/upload/v1735365050/Free_Vector___Flat_design_illustration_customer_support_shac43.jpg"
+            alt="Support Illustration"
+            className="hero-image"
+          />
+        </div>
+        <div className="hero-right">
+          <h2 className="hero-action-title">Get Started</h2>
+          <div className="button-container">
+            <Link to="/signin" className="main-button">
+              Sign In
+            </Link>
+            <Link to="/signup" className="main-button">
+              Sign Up
+            </Link>
+          </div>
+        </div>
       </header>
-      <div className="box-container">
-        <Link to="/signin" className="main-button">
-          Sign In
-        </Link>
-        <Link to="/signup" className="main-button">
-          Sign Up
-        </Link>
-      </div>
       <section className="features-section">
         <h2 className="features-title">Why Choose Us?</h2>
         <div className="features-list">
