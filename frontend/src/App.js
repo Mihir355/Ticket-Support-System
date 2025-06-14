360e887c4c03a6a00a53b68b57255f386869f200
Mihir Biswas <biswasmihir355@gmail.com>
2025-06-14 14:39:46 +0530

    added lazy loading in app.js and added a loader animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,57 @@
-// App.js
-import "./App.css";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import UserDash from "./components/UserDash";
-import CreateTicket from "./components/CreateTicket";
-import ViewMyTickets from "./components/ViewMyTickets";
-import TicketDetails from "./components/TicketDetails";
-import AdminDash from "./components/AdminDash";
-import ViewEmployeeTickets from "./components/ViewEmployeeTickets";
-import EmployeeTicketDetails from "./components/EmployeeTicketDetails";
+
+// Lazy imports
+const Homepage = lazy(() => import("./components/Homepage"));
+const SignIn = lazy(() => import("./components/SignIn"));
+const SignUp = lazy(() => import("./components/SignUp"));
+const UserDash = lazy(() => import("./components/UserDash"));
+const CreateTicket = lazy(() => import("./components/CreateTicket"));
+const ViewMyTickets = lazy(() => import("./components/ViewMyTickets"));
+const TicketDetails = lazy(() => import("./components/TicketDetails"));
+const AdminDash = lazy(() => import("./components/AdminDash"));
+const ViewEmployeeTickets = lazy(() =>
+  import("./components/ViewEmployeeTickets")
+);
+const EmployeeTicketDetails = lazy(() =>
+  import("./components/EmployeeTicketDetails")
+);
+
+// Optional: You can create or import a loader component
+const Loader = () => <div>Loading...</div>;
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/userpage/:userId" element={<UserDash />} />
-        <Route path="/employeepage/:userId" element={<AdminDash />} />
-        <Route
-          path="/userpage/:userId/create-ticket"
-          element={<CreateTicket />}
-        />
-        <Route
-          path="/userpage/:userId/view-tickets"
-          element={<ViewMyTickets />}
-        />
-        <Route
-          path="/userpage/:userId/view-tickets/:ticketId/details"
-          element={<TicketDetails />}
-        />
-        <Route
-          path="/employeepage/:userId/view-tickets"
-          element={<ViewEmployeeTickets />}
-        />
-        <Route
-          path="/employeepage/:userId/view-tickets/:ticketId/details"
-          element={<EmployeeTicketDetails />}
-        />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/userpage/:userId" element={<UserDash />} />
+          <Route path="/employeepage/:userId" element={<AdminDash />} />
+          <Route
+            path="/userpage/:userId/create-ticket"
+            element={<CreateTicket />}
+          />
+          <Route
+            path="/userpage/:userId/view-tickets"
+            element={<ViewMyTickets />}
+          />
+          <Route
+            path="/userpage/:userId/view-tickets/:ticketId/details"
+            element={<TicketDetails />}
+          />
+          <Route
+            path="/employeepage/:userId/view-tickets"
+            element={<ViewEmployeeTickets />}
+          />
+          <Route
+            path="/employeepage/:userId/view-tickets/:ticketId/details"
+            element={<EmployeeTicketDetails />}
+          />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
